6b361da4ae2fec4b020e697fb1a7a067c2a8a26b
Dhananjay Narula <dhananjay0376@gmail.com>
2026-03-09 21:45:29 +0530

    Added size slider for adjusting the slider of node element

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -99,7 +99,7 @@ function json(data, status = 200, origin = '*') {
 function corsHeaders(origin) {
   return {
     'access-control-allow-origin': origin || '*',
-    'access-control-allow-methods': 'GET,POST,PUT,OPTIONS',
+    'access-control-allow-methods': 'GET,POST,PUT,DELETE,OPTIONS',
     'access-control-allow-headers': 'content-type, authorization',
     'access-control-max-age': '86400',
     'vary': 'Origin'
